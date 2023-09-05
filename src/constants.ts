5b5766d33aec103b4980b0d456cf19a52198b29e
Tomáš Reichmann <t_reichmann@wargaming.net>
2023-09-05 15:07:08 +0200

    join link update

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const joinLink = "https://surveys.wargaming.net/s3/Hackathon-Hab-2022";
+export const joinLink = "https://surveys.wargaming.net/s3/Hackathon-Hab-2023";
 export const lookingForATeamLink =
   "https://teams.microsoft.com/l/channel/19%3af09940b209a04ff7a4c82091d51d0b61%40thread.skype/WOTPRG%2520Hackathon%25202022%2520Looking%2520for%2520a%2520team%2520or%2520member?groupId=ac31c2e3-cf31-41ba-8318-c24b47ce6a2b&tenantId=5f98fadd-e7df-4de5-af15-f3da802bab2a";
 export const infoChannelLink =
